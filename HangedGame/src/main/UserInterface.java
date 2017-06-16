e0a1ef17f1b61ec3d41e99e3ad56929475c32c89
alumne <alumne@PC05S21>
2017-06-16 13:12:34 +0200

    Luis UserInterface added showMenuBoard()

---
+++ HangedGame/src/main/UserInterface.java
@@ -0,0 +1,44 @@
+package main;
+
+import util.Input;
+
+
+
+public class UserInterface {
+	public static String showMenuInit(){
+		return null;
+	}
+	
+	public static String showMenuBoard(String wordPlayer, String hint, int attempts){
+		System.out.println("Tablero:");
+		System.out.println("\nPista: " +hint );
+		
+		System.out.println("\n"+wordPlayer);
+		System.out.println("\nIntentos restantes:"+attempts);
+		String valid=null;
+		
+		while(valid==null){
+			String option=Input.scannLine();
+			
+			if(option.toCharArray().length>1){
+				if (option.equals("exit")){
+					valid=option; 
+					break;
+				}else if(option.equals("reset")){
+					valid=option;
+					break;
+				}
+			}else if(option.toCharArray().length==1){
+				valid=option;
+				break;
+			}else{
+				System.out.println(option + " NO es opcion valida");
+			}
+		}
+		return hint;
+	}
+	
+	public static void showMenuAgain(boolean winner){
+		
+	}
+}
